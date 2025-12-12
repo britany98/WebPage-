ab223e44f574dff1ad9136c9bf3451a452ecea05
Britany Rojas Morera <britanyrojas98@gmail.com>
2025-12-12 17:30:40 -0600

    Diseño finalizado

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+window.onscroll = () => {
+    sections.onbeforematch(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' 
+                    + id + ' ]').classList.add('active')
+            })
+        }
+    })
+}
+
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
